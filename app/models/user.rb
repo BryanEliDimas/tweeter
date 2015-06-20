6fc91bb7e035fc32b92f6a056cfb7093c2b09c21
bryanelidimas <bryanelidimas@gmail.com>
2015-06-20 11:55:35 -0500

    Validate uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@.+\.\S+\z/ }
   validates :password, presence: true, length: { minimum: 8 }
-  validates :username, presence: true, length: { minimum: 3 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
 
   has_secure_password
   acts_as_follower
